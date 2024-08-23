2091700d35de99a3df931411ce07cbbac1a52b9a
Tameikat92 <meekestmeek@gmail.com>
2024-08-22 21:16:34 -0400

    aero functions examples added

--- Functions: JavaScript/script.js
+++ Functions: JavaScript/script.js
@@ -16,3 +16,44 @@ function greet(name,greeting){
 greet("Kevin", "Hello");
 greet("Elvis ");
 
+function squareNumber(number){
+    return number * number;
+}
+
+let result = squareNumber(4);
+console.log(result);
+
+function add(a,b){
+    return a + b;
+}
+let sum = add(8,9)
+console.log(sum);
+
+function multiplyAdd(x,y,z){
+    let total = add(x,y);
+     return total * z;
+
+}
+
+let total = multiplyAdd(4,2,2);
+console.log(total);
+
+function helloWorld(){
+    console.log("Hello World");
+}
+
+helloWorld();
+
+// aero function. these are used for basic things
+const sayHello2 = () => { console.log("Hello World Aero function ") };
+
+// calling the aero function
+sayHello2()
+
+// another example of an aero function
+const doubleNumber = (number) => {
+    return number * 2;
+}
+let answer = doubleNumber(5);
+console.log(answer)
+
