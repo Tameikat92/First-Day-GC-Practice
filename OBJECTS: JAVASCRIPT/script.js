245a1115c1964bf50c497061f32d7eb9b57e9830
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-08-29 19:44:21 -0400

    objects file

---
+++ OBJECTS: JAVASCRIPT/script.js
@@ -0,0 +1,16 @@
+document.write("Objects")
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2021,
+    color: "Blue",
+
+    start: function() {
+          console.log("Rev up the engine!!!");
+    }
+
+};
+
+console.log(car.makemake);
+console.log(car.model);
