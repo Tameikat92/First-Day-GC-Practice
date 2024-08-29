ba7152b888f11655aa9d79f75725f2ced1ade661
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-08-29 19:55:33 -0400

    git classes

--- OBJECTS: JAVASCRIPT/script.js
+++ OBJECTS: JAVASCRIPT/script.js
@@ -12,5 +12,11 @@ const car = {
 
 };
 
+car.color = "red"; // can change properties of values to make them more dynamic
+
 console.log(car.makemake);
 console.log(car.model);
+console.log(car.year);
+console.log(car.color);
+
+car.start();
