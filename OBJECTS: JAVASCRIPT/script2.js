8c8abac4ffa0cc2453829f0407d771d518c68fe1
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-08-29 21:11:42 -0400

    more objects

--- OBJECTS: JAVASCRIPT/script2.js
+++ OBJECTS: JAVASCRIPT/script2.js
@@ -18,8 +18,51 @@ const students = [
         name: "Bob",
         age: "21",
         major: "Physics"
-    }
-    
+    }  
 ];
-const student = students[0]
-console.log(students[0].age)
+// students[0].age;
+// const student = students[1];
+//console.log(students[1].age)
+
+for (let i = 0; i < students.length; i++){
+    const student = students[i];
+    console.log(student.age);
+}
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age:34,
+    hobbies: ["reading","cycling","cooking"],
+
+    address: {
+        street: "123 Main Street",
+        city: "Night City",
+        zip: 12345
+    },
+
+    fullName: function(){
+        console.log(this.firstName);
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+const person2 = {
+    firstName: "Amerie",
+    lastName: "Washingtom",
+    age:34,
+    hobbies: ["reading","cycling","cooking"],
+
+    address: {
+        street: "123 Main Street",
+        city: "Night City",
+        zip: 12345
+    },
+
+    fullName: function(){
+        console.log(this.firstName2);
+       // return this.firstName + " " + this.lastName;
+    }
+}
+
+console.log(person.fullName());
