4eb80b2c65157acc5a3a41a0c8b217c7d7134e5d
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-09-05 21:21:41 -0400

    classes in javascript

--- CLASSES JAVASCRIPT/script.js
+++ CLASSES JAVASCRIPT/script.js
@@ -8,7 +8,21 @@ constructor(makeParam,modelParam,yearParam){
     this.model = modelParam;
     this.year = yearParam;
 }
-
+ getCarInfo(){
+   console.log( `${this.year},${this.model},${this.make}`);
 }
-// new is preferencing that I want to make a new object based on class
+}
+
+
+// "new" is preferencing that I want to make a new object based on class
 const myCar = new Car("Toyota","Corolla","2020");
+const myCar2 = new Car("Mercedes","Benz","2021");
+const myCar3 = new Car("Tesla","Model X","2022");
+
+myCar3.getCarInfo();
+
+// let allCars =  [myCar,myCar2,myCar3]
+// console.log(allCars)
+
+let car1 = myCar
+console.log(car1)
